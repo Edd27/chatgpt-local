4846cfe52fc4d3299490add2a9fd4f6eb1426c78
Edgar Benavides <edgarben27@gmail.com>
2024-06-15 15:13:46 -0600

    feat: First commit

---
+++ index.js
@@ -0,0 +1,103 @@
+import { CreateWebWorkerMLCEngine } from "https://esm.run/@mlc-ai/web-llm";
+
+const $ = (element) => document.querySelector(element);
+
+const $form = $("form");
+const $input = $("input");
+const $template = $("#message-template");
+const $messages = $("ul");
+const $container = $("main");
+const $button = $("button");
+const $small = $("small");
+
+let messages = [];
+
+const SELECTED_MODEL = "Llama-3-8B-Instruct-q4f32_1-MLC-1k";
+
+const engine = await CreateWebWorkerMLCEngine(
+  new Worker(new URL("./worker.js", import.meta.url), {
+    type: "module",
+  }),
+  SELECTED_MODEL,
+  {
+    initProgressCallback: (info) => {
+      $small.textContent = info.text;
+
+      if (info.progress === 1) {
+        $button.removeAttribute("disabled");
+        $input.removeAttribute("disabled");
+      }
+    },
+  },
+);
+
+$form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const messageText = $input.value.trim();
+
+  if (messageText === "") return;
+
+  if (messageText !== "") {
+    $input.value = "";
+  }
+
+  const userMessage = {
+    role: "user",
+    content: messageText,
+  };
+
+  messages.push(userMessage);
+
+  addMessage(messageText, "user");
+
+  $button.setAttribute("disabled", true);
+  $input.setAttribute("disabled", true);
+
+  const chunks = await engine.chat.completions.create({
+    messages,
+    stream: true,
+  });
+
+  let reply = "";
+
+  const $botMessage = addMessage("", "bot");
+
+  for await (const chunk of chunks) {
+    const [choice] = chunk.choices;
+    let content = choice?.delta?.content ?? "";
+    reply += content;
+    $botMessage.innerHTML = reply.replace(
+      /```(.*?)```/gs,
+      (math, p1) => `<pre><code>${p1.trim()}</code></pre>`,
+    );
+  }
+
+  messages.push({
+    role: "assistant",
+    content: reply,
+  });
+
+  $button.removeAttribute("disabled");
+  $input.removeAttribute("disabled");
+  $container.scrollTop = $container.scrollHeight;
+});
+
+function addMessage(content, sender) {
+  const cloneTemplate = $template.content.cloneNode(true);
+
+  const $newMessage = cloneTemplate.querySelector(".message");
+  const $span = $newMessage.querySelector("span");
+  const $div = $newMessage.querySelector("div");
+
+  $span.textContent = sender === "bot" ? "GPT" : "Tú";
+  $div.innerHTML = content;
+
+  $newMessage.classList.add(sender);
+
+  $messages.appendChild($newMessage);
+
+  $container.scrollTop = $container.scrollHeight;
+
+  return $div;
+}
